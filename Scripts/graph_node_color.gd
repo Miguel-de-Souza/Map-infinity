c13bc6c32b3f00e8d24b8ca054a57a0a8f30b5f8
Miguel-de-Souza <souzairmaos922++++++++++++++++@gmail.com>
2026-02-24 21:54:48 -0400

    novas funções no bloco de cor
    Veja os códicos R,G,B, e Hex da cor

--- Scripts/graph_node_color.gd
+++ Scripts/graph_node_color.gd
@@ -2,10 +2,19 @@ extends GraphNode
 
 @export var title_line: LineEdit
 @export var size_fonts: SpinBox
-@export var color: ColorPickerButton
+@export var button_color: ColorPickerButton
+
+@export var line_hex: LineEdit
+@export var line_r: LineEdit
+@export var line_g: LineEdit
+@export var line_b: LineEdit
 
 func _ready() -> void:
 	size_fonts.value = Global.font_size_default
+	line_r.text = str(int(button_color.color.r))
+	line_g.text = str(int(button_color.color.g))
+	line_b.text = str(int(button_color.color.b))
+	line_hex.text = str(button_color.color.to_html(false))
 
 	
 func _on_font_size_title_value_changed(value: float) -> void:
@@ -28,9 +37,13 @@ func get_save_data() -> Dictionary:
 	return {
 		"title": title_line.text,
 		"font_size": size_fonts.value,
-		"node_color": [color.color.r, color.color.g, color.color.b, color.color.a], # 👈 ADICIONA ISSO
+		"node_color": [button_color.color.r, button_color.color.g, button_color.color.b, button_color.color.a],
 		"slots_add": slots_add,
-		"slots": slots
+		"slots": slots,
+		"info_color_r": line_r.text,
+		"info_color_g": line_g.text,
+		"info_color_b": line_b.text,
+		"info_color_hex": line_hex.text
 	}
 
 
@@ -40,7 +53,12 @@ func load_save_data(data: Dictionary) -> void:
 	size_fonts.value = data.get("font_size", 14)
 	
 	var saved_color = data.get("node_color", [1,1,1,1])
-	color.color = Color(saved_color[0], saved_color[1], saved_color[2], saved_color[3])
+	button_color.color = Color(saved_color[0], saved_color[1], saved_color[2], saved_color[3])
+
+	line_r.text = data.get("info_color_r",0)
+	line_g.text = data.get("info_color_g",0)
+	line_b.text = data.get("info_color_b",0)
+	line_hex.text = data.get("info_color_hex",0)
 
 	for child in get_children():
 		if child is ColorRect:
@@ -108,3 +126,10 @@ func _on_button_sub_pressed() -> void:
 	size = Vector2(1,1)
 	
 	set_slot(slots_add, false, 0, Color.WHITE, false, 0, Color.WHITE)
+
+
+func _on_color_picker_button_color_changed(color: Color) -> void:
+	line_r.text = str(int(button_color.color.r * 255))
+	line_g.text = str(int(button_color.color.g * 255))
+	line_b.text = str(int(button_color.color.b * 255))
+	line_hex.text = str(button_color.color.to_html(false))
